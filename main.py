dec97d81768b79f77e2267c4b634c99ad34b0e39
wlehmann1 <wade.lehmann@outlook.com>
2025-03-09 13:15:05 -0500

    Final version

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-from stats import number_of_words
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
-    book_path = "books/frankenstein.txt"
-    print(f"--- Begin report of {book_path} ---")
-    print()
-    text = get_book_text(book_path)
-    #print(text)
-    print(f"{number_of_words(text)} words found in the document.")
-    print()
-    letters = character_counts(text)
-    letters.sort(reverse=True, key=sort_on)
-    for item in letters:
-        print(f"The '{item["letter"]}' character was found {item["num"]} times.")
-    print()
-    print("--- End report ---")
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-def sort_on(dict):
-    return dict["num"]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def character_counts(string):
-    result = []
-    letters = {}
-    lowered_string = string.lower()
-    for char in lowered_string:
-        if char.isalpha():
-            letters[char] = letters.get(char, 0) + 1
-    for key, value in letters.items():
-        result.append({"letter": key, "num": value})
-    return result
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
